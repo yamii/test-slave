cde7c76f0eedfa800f1162fc1ba40fe1b4adcaaf
Chad Dumadag <capdumadag@gmail.com>
2016-01-13 17:10:57 +0800

    feature: modify static variables to environment variables
    - Closes TK-07342

--- runner-local.sh
+++ runner-local.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-npm run protractor -- protractor-dd-e2e-local.conf.js --params.templateId=$1
+npm run protractor -- protractor-dd-e2e-local.conf.js --params.templateId=$1 --params.envObj=$2
