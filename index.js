0e121117e71ddce765799d832524850da6494fe5
Miriam Mae Iglesia <miriamiglesia@gmail.com>
2016-01-19 11:27:06 +0800

    feature: create a unique name only once

--- index.js
+++ index.js
@@ -4,10 +4,24 @@ const Slave  = require( './slave' );
 const config = require( './config' );
 const random = require( 'node-random-name' );
 
+// read config name file
+const fs       = require( 'fs' );
+const namePath = process.cwd() + '/config/specs.json';
+let specs      = require( './config/specs.json' );
+let slaveName  = 'slave';
+
+if ( specs.name === 'slave' ) {
+	specs.name =  slaveName = random().replace( ' ', '-' ).toLowerCase();
+	// write updates
+	fs.writeFile( namePath, JSON.stringify( specs ), ( error ) => {
+		if ( error ) console.log( error );
+	} );
+}
+
 const slave = new Slave( {
 	'host' : config.host,
 	'port' : config.port,
-	'name' : random().replace( ' ', '-' ).toLowerCase()
+	'name' : slaveName
 } );
 
 slave.on( 'connected', ( slaveClient ) => {
